6fbe8ce6b05cc1227037e8a1c4f1ff044664beda
macislyfe <mcooke1700@yahoo.com>
2023-07-18 16:25:30 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import PokemonDetails from './pages/PokemonDetails';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/pokemon/:id" element={<PokemonDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
